cbf3e2c5f01a944fd22d6ec97904c7416f91bea1
xiaohui.lam <xiaohui.lam@icloud.com>
2019-06-25 11:06:01 +0800

    fix

--- scripts/mergeRequireDev.php
+++ scripts/mergeRequireDev.php
@@ -3,7 +3,7 @@ $composerJsonFile = __DIR__ . '/../composer.json';
 $composerJson = json_decode(file_get_contents($composerJsonFile), true);
 
 $laravelVer = str_replace('.*', '', $composerJson['require-dev']['laravel/laravel']);
-$laravelVerUrl = 'https://raw.githubusercontent.com/laravel/laravel/' . $laravelVer . '/composer.json';
+$laravelVerUrl = 'https://raw.githubusercontent.com/laravel/laravel/' . ($laravelVer == '5.8' ? 'master' : $laravelVer) . '/composer.json';
 $laravelComposerJson = json_decode(file_get_contents($laravelVerUrl), true);
 
 $composerJson['require-dev'] = array_merge($composerJson['require-dev'], $laravelComposerJson['require-dev']);
