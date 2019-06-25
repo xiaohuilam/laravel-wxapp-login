bef57842492873a59a2d6bfd81385c491daba384
xiaohui.lam <xiaohui.lam@icloud.com>
2019-06-25 10:19:48 +0800

    debug

--- scripts/mergeRequireDev.php
+++ scripts/mergeRequireDev.php
@@ -1,6 +1,8 @@
 <?php
+$fp = fopen(__DIR__ . '/../composer.json', 'r');
+$json = fread($fp, 100000);
 
-$composerJson = json_decode(file_get_contents(__DIR__ . '/../composer.json'), true);
+$composerJson = json_decode($json, true);
 $laravelVer = str_replace('.*', '', $composerJson['require-dev']['laravel/laravel']);
 $laravelVerUrl = 'https://raw.githubusercontent.com/laravel/laravel/' . $laravelVer . '/composer.json';
 echo 'laravelVerUrl = ' . $laravelVerUrl . PHP_EOL;
@@ -8,4 +10,6 @@ $laravelComposerJson = json_decode(file_get_contents($laravelVerUrl), true);
 echo json_encode($laravelComposerJson) . PHP_EOL;
 
 $composerJson['require-dev'] = array_merge($composerJson['require-dev'], $laravelComposerJson['require-dev']);
-file_put_contents(__DIR__ . '/../composer.json', str_replace('\/', '/', json_encode($composerJson)));
+
+$fp = fopen(__DIR__ . '/../composer.json', 'w');
+fwrite($fp, str_replace('\/', '/', json_encode($composerJson)));
