fc5f94c30e22ba61a7434d7dbd8c888e649bf4b7
xiaohui.lam <xiaohui.lam@icloud.com>
2019-06-25 10:27:04 +0800

    debug

--- scripts/mergeRequireDev.php
+++ scripts/mergeRequireDev.php
@@ -1,12 +1,14 @@
 <?php
 $composerJsonFile = __DIR__ . '/../composer.json';
 echo 'composerJsonFile = ' . $composerJsonFile . PHP_EOL;
-echo 'exists = ' . var_export(file_exists($composerJsonFile), 1) . PHP_EOL;
 
 $fp = fopen($composerJsonFile, 'r');
 $json = fread($fp, 100000);
+echo 'json = ' . var_export($json, 1) . PHP_EOL;
 
 $composerJson = json_decode($json, true);
+echo 'json_decode = ' . var_export($composerJson, 1) . PHP_EOL;
+
 $laravelVer = str_replace('.*', '', $composerJson['require-dev']['laravel/laravel']);
 $laravelVerUrl = 'https://raw.githubusercontent.com/laravel/laravel/' . $laravelVer . '/composer.json';
 echo 'laravelVerUrl = ' . $laravelVerUrl . PHP_EOL;
