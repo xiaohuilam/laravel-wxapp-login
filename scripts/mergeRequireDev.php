bb21af7de0ca3eba60062b4b94196e55c1baa0c3
xiaohui.lam <xiaohui.lam@icloud.com>
2019-06-25 16:47:05 +0800

    enhancement

--- scripts/mergeRequireDev.php
+++ scripts/mergeRequireDev.php
@@ -1,11 +1,14 @@
 <?php
-$latestLaravelVerContent = file_get_contents('https://api.github.com/repos/xiaohuilam/laravel-wxapp-login/issues/10', false, stream_context_create([
+$opt = stream_context_create([
     'http' => [
         'method' => "GET",
         'header' => "Accept-language: en\r\n" .
-            "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36\r\n"
+            "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36\r\n" .
+            "Authorization: token " . getenv('GITHUB_TOKEN') . "\r\n"
     ],
-]));
+]);
+
+$latestLaravelVerContent = file_get_contents('https://api.github.com/repos/xiaohuilam/laravel-wxapp-login/issues/10', false, $opt);
 $json = json_decode($latestLaravelVerContent);
 
 if (json_last_error() !== JSON_ERROR_NONE || !$latestLaravelVerContent || !$json || !isset($json->body)) {
@@ -19,7 +22,7 @@ $composerJson = json_decode(file_get_contents($composerJsonFile), true);
 
 $laravelVer = str_replace('.*', '', $composerJson['require-dev']['laravel/laravel']);
 $laravelVerUrl = 'https://raw.githubusercontent.com/laravel/laravel/' . ($laravelVer == $ver ? 'master' : $laravelVer) . '/composer.json';
-$laravelComposerJson = json_decode(file_get_contents($laravelVerUrl), true);
+$laravelComposerJson = json_decode(file_get_contents($laravelVerUrl, false, $opt), true);
 
 $composerJson['require-dev'] = array_merge($composerJson['require-dev'], $laravelComposerJson['require-dev']);
 
